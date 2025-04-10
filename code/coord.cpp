ba6c4a501e6283dbb2e99a78c3287d63ded73002
Quentin T <108391629+wowskii@users.noreply.github.com>
2025-04-10 13:28:30 +0000

    Debut Coordonnees

--- code/coord.cpp
+++ code/coord.cpp
@@ -1,3 +1,30 @@
 #include "doctest.h"
 #include "coord.hpp"
+#include <iostream>
 
+using namespace std;
+
+int TAILLEGRILLE = 40;
+
+
+
+Coord::Coord(int coordx, int coordy) : x{coordx}, y{coordy}
+{
+    if (x < 0 or y < 0 or x > 40 or x > 40) throw invalid_argument("Coordonnées invalides");
+}
+
+int Coord::getX(){
+    return x;
+}
+
+int Coord::getY(){
+    return y;
+}
+
+TEST_CASE("Constructeur") {
+    Coord c1{4,5};
+    CHECK(c1.getX() == 4);
+    CHECK(c1.getY() == 5);
+
+    CHECK_THROWS_AS(Coord(50, 30), invalid_argument);
+}
---
+++ code/coord.hpp
@@ -0,0 +1,15 @@
+
+
+class Coord
+{
+private:
+    int x = 0;
+    int y = 0;
+
+public:
+    Coord(int x, int y);
+
+    int getX();
+    int getY();
+};
+
