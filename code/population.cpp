edb1a06d55043ffbc9faa991672aa7fc4e8664d9
wowskii <quentutu2@gmail.com>
2025-04-26 13:35:19 +0200

    commencé la classe Population

---
+++ code/population.cpp
@@ -0,0 +1,32 @@
+#include "population.hpp"
+#include "doctest.h"
+
+Population::Population() {
+    vector<Animal> t = {};
+}
+
+Animal Population::get(int id) const {
+    for (int i = 0; i < t.size(); i++) {
+        if (t[i].getId() == id) return t[i];
+    }
+    return Animal();
+}
+
+
+Ensemble Population::getIds() const {
+    Ensemble e;
+    for (auto a : t) {
+        e.ajoute(a.getId());
+    }
+    return e;
+}
+
+
+TEST_CASE("getIds") {
+    Population p();
+}
+
+
+int Population::reserve() {
+    return 0;
+}
---
+++ code/population.hpp
@@ -0,0 +1,29 @@
+#ifndef POPULATION_HPP
+#define POPULATION_HPP
+
+#include "coord.hpp"
+#include "animal.hpp"
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include <sstream>
+
+using namespace std;
+
+class Population {
+    private:
+    vector<Animal> t;
+
+    public:
+    Population();
+    
+    Animal get(int id) const;
+    Ensemble getIds() const;
+
+    int reserve();
+    void set();
+    void supprime(int id);
+};
+
+#endif
