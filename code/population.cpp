57571aea2afe0d8b1773e73d486909f0ef956871
wowskii <quentutu2@gmail.com>
2025-05-01 17:49:13 +0200

    tests population

--- code/population.cpp
+++ code/population.cpp
@@ -22,6 +22,19 @@ Ensemble Population::getIds() const
     return e;
 }
 
+TEST_CASE("getIds") {
+    Population p;
+    int id;
+    id = p.reserve();
+    p.set(id, Animal(id, Lapin, Coord(5,15)));
+    id = p.reserve();
+    p.set(id, Animal(id, Renard, Coord(5,15)));
+    id = p.reserve();
+    p.set(id, Animal(id, Lapin, Coord(5,15)));
+    CHECK(p.getIds().cardinal() == 3);
+    //cout << p.getIds() << endl;
+}
+
 int Population::reserve()
 {
     while (map.find(nextId) != map.end())
@@ -44,7 +57,30 @@ void Population::set(int id, const Animal &animal)
     }
 }
 
+TEST_CASE("get, set et reserve") {
+    Population p;
+    int id = p.reserve();
+    p.set(id, Animal(id, Renard, Coord(2,4)));
+
+    CHECK(p.get(id).getId() == id);
+    CHECK(p.get(id).getEspece() == Espece::Renard);
+    CHECK(p.get(id).getCoord() == Coord(2,4));
+}
+
 void Population::supprime(int id)
 {
     map.erase(id);
+}
+
+TEST_CASE("supprime") {
+    Population p;
+    int id;
+    id = p.reserve();
+    p.set(id, Animal(id, Lapin, Coord(5,15)));
+    id = p.reserve();
+    p.set(id, Animal(id, Renard, Coord(5,15)));
+    id = p.reserve();
+    p.set(id, Animal(id, Lapin, Coord(5,15)));
+    p.supprime(id);
+    CHECK(p.getIds().cardinal() == 2);
 }
